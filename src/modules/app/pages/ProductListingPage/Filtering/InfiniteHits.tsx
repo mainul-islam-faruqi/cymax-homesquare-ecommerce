782f4d210766ea8256eb017a8762cc97bf309a2b
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2024-02-16 17:54:01 +0600

    filters updated with back fnctionality

--- src/modules/app/pages/ProductListingPage/Filtering/InfiniteHits.tsx
+++ src/modules/app/pages/ProductListingPage/Filtering/InfiniteHits.tsx
@@ -1,7 +1,8 @@
 import { Button, Flex, Grid, GridItem, Text, VStack } from '@chakra-ui/react'
+import { useSessionStorage } from '@modules/app/hooks/useSessionStorage'
 import { useGetMultipleStocks } from '@modules/ep'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import { useInfiniteHits } from 'react-instantsearch-hooks-web'
 import { useIntl } from 'react-intl'
 import { ALGOLIA_BASE_INDEX } from '../../../../algolia'
@@ -17,78 +18,151 @@ import { searchClient } from '../'
  */
 export interface InfiniteHitsProps {
   filters: string
+  searchQuery: string
 }
 
 export const InfiniteHits = (props: InfiniteHitsProps) => {
   const index = searchClient.initIndex(ALGOLIA_BASE_INDEX)
-
-  // Function to load products up to a specified page
-
+  const { getItem, setItem } = useSessionStorage()
   const router = useRouter()
-  const [currPage, setCurrPage] = useState<number>(1)
-  const { hits, isLastPage, showMore, results } = useInfiniteHits()
-  const { gridTemplateColumns } = useGridLayout()
-  const { page, nbPages } = results || {}
+  const [routeChanged, setRouteChanged] = useState<Boolean>(false)
+  const [isShowMore, setIsShowMore] = useState<Boolean>(false)
+  const [isPopstate, setIsPopstate] = useState<Boolean>(false)
+  const [detailClicked, setDetailClicked] = useState<Boolean>(false)
+  const { hits, currentPageHits, isLastPage, showMore, results } =
+    useInfiniteHits()
+  const { gridTemplateColumns, hitsPerPage } = useGridLayout()
+  const { page, nbPages, nbHits } = results || {}
 
-  console.log('page', page)
-  // console.log('currentPageHits', currentPageHits)
   const [allProducts, setAllProducts] = useState<any[]>([])
 
+  // useEffect to handle popstate events for route changes
+  useEffect(() => {
+    const handlePopstate = (event: any) => {
+      setIsShowMore(false)
+      setIsPopstate(true)
+
+      const targetUrl = event.state
+        ? (event.state as { url: string }).url === '/?'
+          ? '/'
+          : (event.state as { url: string }).url
+        : event.currentTarget instanceof Window &&
+          event.currentTarget.history?.state?.as
+
+      if (router) {
+        console.log('Event:', event)
+        router.push(targetUrl)
+      }
+    }
+    window.addEventListener('popstate', handlePopstate)
+    return () => {
+      // Cleanup: Remove the event listener when the component is unmounted
+      window.removeEventListener('popstate', handlePopstate)
+    }
+  }, [router])
+
+  // Effect to handle detailClicked state stored in session storage mounting from PDP to PLP
+  useEffect(() => {
+    const detailClicked = getItem('detailClicked')
+    setDetailClicked(detailClicked)
+
+    return () => {
+      setItem('detailClicked', false)
+    }
+  }, [getItem, setItem])
+
+  // Effect to fetch from Algolia and manage product data upto current page
+  // based on different conditions (pages, routeChanged, isShowMore, etc.)
   useEffect(() => {
     async function getAllProductsUpToCurrentPage(currentPageNumber: number) {
       const allProducts = []
-      console.log('currentPageNumber', currentPageNumber)
+
       for (let page = 0; page <= currentPageNumber; page++) {
-        const { hits } = await index.search('', {
-          filters: props.filters,
-          page: page,
-          hitsPerPage: 24,
-        })
-        allProducts.push(...hits)
+        const { hits: indexSearchResult } = await index.search(
+          props.searchQuery ? props.searchQuery : '',
+          {
+            filters: props.filters,
+            page: page,
+            hitsPerPage: 24,
+          }
+        )
+
+        allProducts.push(...indexSearchResult)
       }
 
       return allProducts
     }
     const fetchData = async () => {
       try {
-        const products = await getAllProductsUpToCurrentPage(page ?? 0)
-        console.log('products', products)
-        setAllProducts(products)
-        // setAllProducts(products);
+        console.log('router', router)
+        if (
+          routeChanged &&
+          !isShowMore &&
+          !isPopstate &&
+          !detailClicked &&
+          !router.query.page
+        ) {
+          setAllProducts([...hits])
+          setRouteChanged(false)
+          console.log(routeChanged, 'routeChanged')
+          console.log(
+            'routeChanged && !isShowMore && !isPopstate && !detailClicked',
+            router
+          )
+        } else if (
+          Object.keys(router.query).length > 1 &&
+          !isShowMore &&
+          !isPopstate &&
+          !detailClicked &&
+          !router.query.page
+        ) {
+          // This block for filters
+          setAllProducts([...hits])
+          console.log(
+            '(Object.keys(router.query).length > 1 && !isShowMore && !isPopstate && !detailClicked',
+            router,
+            nbHits,
+            hits
+          )
+        } else if (!isShowMore && (isPopstate || detailClicked)) {
+          const products = await getAllProductsUpToCurrentPage(page ?? 0)
+
+          setAllProducts(products)
+          setIsPopstate(false)
+        } else if (
+          isShowMore &&
+          page &&
+          page === allProducts.length / 24 &&
+          (router.query.page ? Number(router.query.page) : 1) === page + 1
+        ) {
+          console.log(allProducts, currentPageHits, 'isShowMore nbHits')
+          console.log(nbHits, 'isShowMore nbHits')
+          setAllProducts((prevProducts) => [
+            ...prevProducts,
+            ...currentPageHits,
+          ])
+          setIsShowMore(false)
+        } else if (!isShowMore && page && page + 1 === hits.length / 24) {
+          console.log(hits.length, page + 1, 'hits page + 1')
+          console.log(nbHits, page, '!isShowMore nbHits page')
+          setAllProducts([...hits])
+        } else if (router.query.page && Object.keys(router.query).length > 2) {
+          setAllProducts([...hits])
+          console.log(hits.length, hits, results, 'hits results')
+        } else if (!isShowMore) {
+          console.log(nbHits, 'else ')
+          const products = await getAllProductsUpToCurrentPage(page ?? 0)
+          console.log(products, ' else products')
+          setAllProducts(products)
+        }
       } catch (error) {
         console.error('Error fetching products:', error)
       }
     }
 
     fetchData()
-  }, [page])
-
-  // Use allProducts to render your UI or perform other actions
-  console.log('allProducts', allProducts)
-  console.log('Hits', hits)
-
-  const loadProductsUpToPage = async (page: number) => {
-    let currentPage = 0
-
-    // Keep fetching until the current page reaches the target page
-    while (currentPage < page ?? 1) {
-      // showMore()
-      currentPage += 1
-    }
-  }
-
-  // Load all products up to page 3 on initial render
-  useEffect(() => {
-    // Use a regular expression to match the page parameter and extract the number
-    const match = router.asPath.match(/(?:\?|&)page=(\d+)/)
-
-    // Check if there's a match and extract the page number
-    const pageNumber = match ? parseInt(match[1], 10) : 0
-
-    console.log(pageNumber) // Output: 5
-
-    loadProductsUpToPage(pageNumber)
-  }, [page])
+  }, [routeChanged, page, isPopstate, detailClicked, isShowMore, router, hits])
+  // console.log('166', allProducts)
   const intl = useIntl()
   const objectIds = hits.map((product, index) => {
     return { id: product.objectID }
@@ -124,31 +198,81 @@ export const InfiniteHits = (props: InfiniteHitsProps) => {
       },
     },
   }
-  const handleShowMore = () => {
-    // Increment the page count
-    const nextPage = currPage + 1
 
-    // Update the URL with the new page parameter
-    router.push(`/${router.asPath.split('?')[0]}?page=${nextPage}`, undefined, {
-      shallow: true,
-    })
+  // Effect to handle route changes, skipping the pages route, setting routeChanged state
+  useEffect(() => {
+    if (!Boolean(Number(router.query.page))) {
+      setRouteChanged(true)
+    }
+    const handleRouteChangeStart = (url: string) => {
+      console.log(url)
+      if (!url.includes('page')) {
+        setRouteChanged(true)
+      }
 
-    // Update the local state
-    setCurrPage(nextPage)
+      if (url.includes('page')) {
+        window.history.replaceState(null, '', url)
+      }
+    }
 
-    // Trigger the showMore method
-    showMore()
-  }
+    // Subscribe to the router events
+    router.events.on('routeChangeStart', handleRouteChangeStart)
+
+    // Cleanup the subscriptions when the component is unmounted
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart)
+
+      setRouteChanged(false)
+    }
+  }, [router])
+
+  // useLayoutEffect to manage and restore scroll position
+  useLayoutEffect(() => {
+    let scrollPositionKey = ''
+    let savedScrollPosition = 0
+    if (router.asPath.includes('query')) {
+      scrollPositionKey = router.asPath.split('&')[0]
+    } else if (router.asPath.includes('?page')) {
+      scrollPositionKey = router.asPath.split('?')[0]
+    } else if (router.query.slug) {
+      scrollPositionKey = router.query.slug[0]
+    } else {
+      scrollPositionKey = router.asPath
+    }
+
+    function updateScroll() {
+      savedScrollPosition = Math.ceil(
+        window.scrollY ? window.scrollY : savedScrollPosition
+      )
+
+      sessionStorage.setItem(scrollPositionKey, savedScrollPosition.toString())
+    }
+
+    // restoring scroll position if any from session storage after loading all products
+    // Also work for back and forward navigation
+    if (allProducts.length > 4) {
+      const scrollPosition = sessionStorage.getItem(scrollPositionKey)
+      if (scrollPosition) {
+        savedScrollPosition = parseInt(scrollPosition)
+        window.scrollTo(0, savedScrollPosition)
+      }
+      window.addEventListener('scroll', updateScroll)
+    }
+
+    const removeEventListeners = () => {
+      window.removeEventListener('scroll', updateScroll)
+    }
+
+    return () => {
+      removeEventListeners()
+    }
+  }, [hits, allProducts.length, router])
 
   return (
     <>
-      {/* add canonical link  */}
-      {/* <Head>
-        <link rel="canonical" href={window.location.href} />
-      </Head> */}
       <VStack w="full">
         <Grid width="full" gridTemplateColumns={gridTemplateColumns}>
-          {hits.map((product, index) => {
+          {allProducts.map((product, index) => {
             return (
               <GridItem
                 key={product.objectID}
@@ -171,8 +295,8 @@ export const InfiniteHits = (props: InfiniteHitsProps) => {
               {intl.formatMessage(
                 { id: 'productListingPage.pageNo' },
                 {
-                  pageNo: page + 1,
-                  total: nbPages,
+                  pageNo: page ? page + 1 : hits.length / hitsPerPage,
+                  total: Math.ceil((nbHits ?? 1) / hitsPerPage),
                 }
               )}
             </Text>
@@ -182,7 +306,7 @@ export const InfiniteHits = (props: InfiniteHitsProps) => {
               height={10}
               fontSize="desktop.bodySM"
               onClick={() => {
-                setCurrPage((currentPage) => currentPage + 1)
+                setIsShowMore(true)
                 showMore()
               }}
             >
